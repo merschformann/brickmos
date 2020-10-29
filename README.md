3f1784d85fbb7ff7fb877e0784e6d95586184bf9
Marius Merschformann <marius.merschformann@gmail.com>
2020-10-29 18:23:37 +0100

    some more documentation and cleaning up a little

--- README.md
+++ README.md
@@ -11,10 +11,44 @@ I wrote this for myself for a birthday present I worked on, but wanted to leave 
 ## Prerequisites
 
 - Python (I used python 3.8)
-  - opencv-python (the version I used is defined in requirements.txt)
+  - opencv-python (the version I used is defined in _requirements.txt_)
 
 ## Process an image
 
 Convert an image by invoking the script as follows:
 
 `python mosaic/brickify.py data/iron-man-portrait.jpg --output_directory="temp"`
+
+## Parameters
+
+Find a short explanation of the parameter arguments below.
+
+#### Required arguments
+- image_file: the path to the (original) image to process, can be a _.jpg_ or _.png_ file
+
+#### Optional arguments
+- color_file: the csv-file defining the brick-colors to be used (if not given, colors.csv at script location is attempted), see format description below
+- output_directory: the directory the output image and BOM is written to (default is current working directory)
+- spares: the number of spares to add per color/brick (in bricklink BOM), just in case of loosing some bricks
+
+## Color definition
+
+The set of available colors is defined by the csv-file (`color_file`, see above). The following information is required.
+
+```
+rgb         ; Bricklink Color Name ; Bricklink Color ID ; Bricklink Part ID
+255,255,255 ; White                ; 1                  ; 3024
+175,181,199 ; LightBluishGray      ; 86                 ; 3024
+89,93,96    ; DarkBluishGray       ; 85                 ; 3024
+33,33,33    ; Black                ; 11                 ; 3024
+```
+
+This is an example set of colors for a 4 'color' black & white picture using 1x1 plates.
+The example is already in expected format. The format requires a header line and semicolons as column delimiters.
+The values used here should be aligned with the definitions on bricklink for the export to work properly (see [https://www.bricklink.com/catalogColors.asp](https://www.bricklink.com/catalogColors.asp)).
+
+More details about each column:
+- The first column is the RGB value of the corresponding color. There is no official definition of these from Lego, thus, use something as close as possible you can find. I picked the colors defined on the bricklink page.
+- The second column is just a recognizable name for the color. Using the bricklink name seems reasonable.
+- The third column is the bricklink color ID (see page mentioned before). This is important for the XML-export to function properly.
+- The fourth column is the bricklink brick type ID. You can find these in bricklinks database. Since the script only considers 1x1 parts, only these should be used. The _3024_ from the example is the 1x1 plate (see [here](https://www.bricklink.com/v2/catalog/catalogitem.page?P=3024#T=C)). Other possible options include, but are not limited to, 1x1 tiles (3070b, see [here](https://www.bricklink.com/v2/catalog/catalogitem.page?P=3070b#T=C)) or 1x1 plate round (4073, see [here](https://www.bricklink.com/v2/catalog/catalogitem.page?P=4073#T=C)). Note that prices vary *a lot* with color **AND** brick type. For my own mosaic I used 1x1 plates, but replaced an expensive color with a much cheaper 1x1 tile. 
--- mosaic/brickify.py
+++ mosaic/brickify.py
@@ -9,6 +9,10 @@ import xml.etree.cElementTree as xmlET
 
 
 class BrickColor:
+    """
+    Defines a brick color with all information needed for processing and bricklink export.
+    """
+
     def __init__(self, rgb, color_name, color_id, color_type):
         self.rgb = rgb
         self.colorName = color_name
@@ -20,12 +24,71 @@ class BrickColor:
 
 
 class BrickColorStats:
+    """
+    Tracks some stats for the corresponding color.
+    """
+
     def __init__(self, brick_color):
         self.brickColor = brick_color
         self.count = 0
 
 
-def closest_color(rgb: Tuple[int, int, int], color_range: list, stats: dict) -> BrickColor:
+def init_and_get_arguments():
+    """
+    Creates an argument parser, handles it and returns the prepared arguments.
+    :return: The arguments.
+    """
+    parser = argparse.ArgumentParser(description="'Mosaicify' an image using brick colors and export bill of material")
+    parser.add_argument('image_file', type=str, help='the image to process')
+    parser.add_argument('--color_file', type=str, default='',
+                        help='the csv-file defining the brick-colors to be used (if not given, colors.csv at script '
+                             'location is attempted)')
+    parser.add_argument('--output_directory', type=str, default='.',
+                        help='the directory the output image and BOM is written to (default is current working dir)')
+    parser.add_argument('--spares', type=int, default=0,
+                        help='the number of spares to add per color/brick (bricklink), just in case of loosing some bricks')
+    # Retrieve arguments
+    args = parser.parse_args()
+    # Use default color definition location, if not given
+    default_color_file = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), 'colors.csv')
+    if args.color_file == '':
+        if not os.path.exists(default_color_file):
+            print('No color definitions found, exiting ...')
+            sys.exit(-1)
+        args.color_file = default_color_file
+    # Quick check for image file presence
+    if not os.path.exists(args.image_file):
+        print(f'Cannot find image file at {args.image_file}, exiting ...')
+        sys.exit(-2)
+    # Create output directory, if it does not exist
+    os.makedirs(args.output_directory, exist_ok=True)
+    return args
+
+
+def read_colors(color_file: str) -> []:
+    """
+    Reads the color file and returns the color definitions.
+    Expected CSV-format (with header, integer RGB): red,green,blue;color-name,bricklink-color-id,bricklink-brick-type
+    :param color_file: The path to the color file.
+    :return: All color definitions read from the file.
+    """
+    colors_read = []
+    with open(color_file) as color_def_file:
+        csv_reader = csv.reader(color_def_file, delimiter=';')
+        next(csv_reader)
+        for row in csv_reader:
+            colors_read.append(BrickColor(tuple([int(i) for i in row[0].split(sep=',')]),
+                                          row[1].strip(), row[2].strip(), row[3].strip()))
+    return colors_read
+
+
+def closest_color(rgb: Tuple[int, int, int], color_range: list) -> BrickColor:
+    """
+    Returns the color from the range which is closest to the given one.
+    :param rgb: The given color to fine the closest one for.
+    :param color_range: The range of possible colors.
+    :return: The color closest to the given one.
+    """
     # Get rgb values of color while adhering to cv2 BGR representation
     r, g, b = rgb
     # Assess euclidean distance of color to all brick colors given
@@ -35,12 +98,27 @@ def closest_color(rgb: Tuple[int, int, int], color_range: list, stats: dict) -> 
         color_diff = math.sqrt(abs(r - cr) ** 2 + abs(g - cg) ** 2 + abs(b - cb) ** 2)
         color_diffs.append((color_diff, color))
     # Get color closest to the given one and update its stats
-    closest = min(color_diffs, key=lambda x: x[0])
-    if closest[1] not in stats:
-        stats[closest[1]] = BrickColorStats(closest[1])
-    stats[closest[1]].count = stats[closest[1]].count + 1
-    # Return it
-    return closest[1]
+    return min(color_diffs, key=lambda x: x[0])[1]
+
+
+def replace_with_brick_colors(img, brick_colors):
+    """
+    Replaces all colors in the image with the closest brick colors given.
+    :param img: The image to process.
+    :return: The color statistics collected in the process.
+    """
+    # Replace with nearest colors from pallet
+    stats = {}
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            # Get and set nearest color for pixel
+            closest = closest_color(img[i, j][::-1], brick_colors)
+            img[i, j] = closest.rgb[::-1]
+            # Update color stats
+            if closest not in stats:
+                stats[closest] = BrickColorStats(closest)
+            stats[closest].count = stats[closest].count + 1
+    return stats
 
 
 def write_xml(filename, stats, spares_per_lot=5):
@@ -63,39 +141,12 @@ def write_xml(filename, stats, spares_per_lot=5):
     tree.write(filename)
 
 
-# Handle arguments
-parser = argparse.ArgumentParser(description="'Mosaicify' an image using brick colors and export bill of material")
-parser.add_argument('image_file', type=str, help='the csv-file defining the brick-colors to be used')
-parser.add_argument('--color_file', type=str, default='',
-                    help='the csv-file defining the brick-colors to be used (if not given, colors.csv at script '
-                         'location is attempted)')
-parser.add_argument('--output_directory', type=str, default='.',
-                    help='the directory the output image and BOM is written to (default is current working dir)')
-parser.add_argument('--spares', type=int, default=0,
-                    help='the number of spares to add per color/brick (bricklink), just in case of loosing some bricks')
-args = parser.parse_args()
-# Use default color definition location, if not given
-default_color_file = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), 'colors.csv')
-if args.color_file == '':
-    if not os.path.exists(default_color_file):
-        print('No color definitions found, exiting ...')
-        sys.exit(-1)
-    args.color_file = default_color_file
-# Quick check for image file presence
-if not os.path.exists(args.image_file):
-    print(f'Cannot find image file at {args.image_file}, exiting ...')
-    sys.exit(-2)
-# Create output directory, if it does not exist
-os.makedirs(args.output_directory, exist_ok=True)
+# ===> Main start
+# Get arguments
+args = init_and_get_arguments()
 
 # Read color info - find colors here: https://www.bricklink.com/catalogColors.asp
-# Expected CSV-format (with header, integer RGB): red,green,blue;color-name,bricklink-color-id,bricklink-brick-type
-colors = []
-with open(args.color_file) as color_def_file:
-    csv_reader = csv.reader(color_def_file, delimiter=';')
-    next(csv_reader)
-    for row in csv_reader:
-        colors.append(BrickColor(tuple([int(i) for i in row[0].split(sep=',')]), row[1], row[2], row[3]))
+colors = read_colors(args.color_file)
 
 # Input image
 image_input = cv2.imread(args.image_file)
@@ -109,16 +160,13 @@ w_out, h_out = (1000, 1000)
 
 # Resize input to "pixelated" size
 image_pixelated = cv2.resize(image_input, (w, h), interpolation=cv2.INTER_LINEAR)
-image_lego = image_pixelated.copy()
+image_bricks = image_pixelated.copy()
 
-# Replace with nearest colors from pallet
-statistics = {}
-for i in range(image_lego.shape[0]):
-    for j in range(image_lego.shape[1]):
-        image_lego[i, j] = closest_color(image_lego[i, j][::-1], colors, statistics).rgb[::-1]
+# Replace with brick colors
+statistics = replace_with_brick_colors(image_bricks, colors)
 
 # Initialize output image
-image_output = cv2.resize(image_lego, (w_out, h_out), interpolation=cv2.INTER_NEAREST)
+image_output = cv2.resize(image_bricks, (w_out, h_out), interpolation=cv2.INTER_NEAREST)
 
 # Show some statistics
 print(f"Colors ({len(statistics)} colors, {sum([i.count for i in statistics.values()])} tiles):")
